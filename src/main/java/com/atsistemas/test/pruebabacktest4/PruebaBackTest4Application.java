91f45ee2331c9ec398bc5c54a26e4a40c6aef7e8
rchafino <rchafino@atsistemas.com>
2022-08-22 12:47:28 +0200

    Initial project structure

---
+++ src/main/java/com/atsistemas/test/pruebabacktest4/PruebaBackTest4Application.java
@@ -0,0 +1,13 @@
+package com.atsistemas.test.pruebabacktest4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBackTest4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBackTest4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/atsistemas/test/pruebabacktest4/PruebaBackTest4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsistemas.test.pruebabacktest4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBackTest4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
